7e1ae9a2825fadc829309e258bc3938ecf88cde8
Cristel Chandre <cchandre@gmail.com>
2021-07-31 15:08:02 +0200

    Reorganization of the files
    - modules (save_data, run_method) in gc2d_modules
    - one file containing the two classes GC2Dt for the turbulent potential, and GC2Dk for the Kryukov potential

--- dict_gc2d.py
+++ dict_gc2d.py
@@ -1,14 +1,14 @@
-## Definition of the parameters for GC2D
+## Definition of the parameters for GC2Ds
 ##
+## potential: string; 'KMdCN' for the Kryukov-Martinell-delCastilloNegrete potential, 'turbulent' for the turbulent potential
 ## M: integer; number of modes
-## N: integer; number of points in each direction of the 2D grid
 ## A: float; amplitude of the electrostatic potential
 ## FLR: boolean array of length 2; Finite Larmor Radius effects for GC order 1 and order 2
 ## flr_order: array of length 2; 'all' or integer; FLR order for each GC order
 ## rho: float; Larmor Radius
 ## gc_order: 1 or 2
 ## eta: float; amplitude of the GC order 2 potential
-## method: string; 'plot_potentials', 'diffusion', 'poincare'
+## method: string; diffusion', 'poincare'
 ## modulo: boolean; only for method='poincare'; if True, x and y are taken modulo 2*pi
 ## Ntraj: integer; number of trajectories to be integrated
 ## Tf: integer; number of periods for the integration of the traejctories
@@ -16,30 +16,31 @@
 ## save_results: boolean; if True, the results are saved in a .mat file
 ## plot_results: boolean; if True, the results are plotted right after the computation
 
-array = [0.01, 0.03, 0.05, 0.07, 0.09]
-iteratable = 'rho'
+potential = 'KMdCN'
 
-dict_list = [{'M': 25} for _ in range(len(array))]
+array = [0.4, 0.5, 0.6]
+iteratable = 'A'
+
+dict_list = [{'potential': potential} for _ in range(len(array))]
 for it in range(len(array)):
 	dict_list[it].update({iteratable: array[it]})
 
 for dict in dict_list:
 	dict.update({
-		'N': 2 ** 10, 
-		'A': 1.0,
-#		'rho': 0.7,
-		'FLR': [True, False],
+		'M': 5,
+		'FLR': [False, False],
     	'flr_order': ['all', 'all'],
+    	'rho': 0.7,
     	'gc_order': 1,
-    	'eta': 0.0})
+    	'eta': 0.1})
 
 for dict in dict_list:
 	dict.update({
     	#'method': 'poincare',
     	'method': 'diffusion',
-    	'modulo': False,
-    	'Ntraj': 2000,
-	    'Tf': 5000,
-    	'timestep': 0.05,
+    	'modulo': True,
+    	'Ntraj': 1000,
+	    'Tf': 1000,
+    	'timestep': 0.03,
     	'save_results': True,
     	'plot_results': False})
--- gc2d.py
+++ gc2d.py
@@ -1,30 +1,13 @@
 import numpy as xp
 from numpy.fft import fft2, ifft2, fftfreq
-import matplotlib.pyplot as plt
 from scipy.interpolate import interpn
-from scipy.integrate import solve_ivp
-from scipy.special import jv
+from scipy.special import jv, eval_chebyu
 import sympy as sp
-from scipy.stats import linregress
-from scipy.io import savemat
-import time
-from datetime import date
+from gc2d_modules import run_method
 from dict_gc2d import dict_list
 
 def main():
 	for dict_params in dict_list:
-		case = GC2D(dict_params)
-		run_method(case)
-
-
-class GC2D:
-	def __repr__(self):
-		return '{self.__class__.__name__}({self.DictParams})'.format(self=self)
-
-	def __str__(self):
-		return '2D Guiding Center ({self.__class__.__name__}) for the turbulent potential with FLR = {self.flr_order} and GC order = {self.gc_order}'.format(self=self)
-
-	def __init__(self, dict_params):
 		if not dict_params['FLR'][0]:
 			dict_params['flr_order'][0] = 0
 		if not dict_params['FLR'][1]:
@@ -33,6 +16,21 @@ class GC2D:
 			dict_params['rho'] = 0
 		if dict_params['gc_order'] == 1:
 			dict_params['eta'] = 0
+		if dict_params['potential'] == 'KMdCN':
+			case = GC2Dk(dict_params)
+		elif dict_params['potential'] == 'turbulent':
+			case = GC2Dt(dict_params)
+		run_method(case)
+
+
+class GC2Dt:
+	def __repr__(self):
+		return '{self.__class__.__name__}({self.DictParams})'.format(self=self)
+
+	def __str__(self):
+		return '2D Guiding Center ({self.__class__.__name__}) for the turbulent potential with FLR = {self.flr_order} and GC order = {self.gc_order}'.format(self=self)
+
+	def __init__(self, dict_params):
 		for key in dict_params:
 			setattr(self, key, dict_params[key])
 		self.DictParams = dict_params
@@ -96,71 +94,56 @@ class GC2D:
 			return dy_gc1 + dy_gc2
 
 
-def run_method(case):
-	print('\033[92m    {} \033[00m'.format(case.__str__()))
-	print('\033[92m    A = {}   rho = {}   eta = {} \033[00m'.format(case.A, case.rho, case.eta))
-	filestr = ('A' + str(case.A) + '_FLR' + str(case.rho) + '_GC' + str(case.gc_order)).replace('.', '')
-	if case.gc_order == 2:
-		filestr += ('_eta' + str(case.eta)).replace('.', '')
-	if case.method == 'plot_potentials':
-		data = xp.array([case.phi, case.phi_, case.phi_gc2_0, case.phi_gc2_2])
-		save_data(case, 'potentials', data, filestr)
-		if case.plot_results:
-			plt.figure(figsize=(8, 8))
-			plt.pcolor(case.xv, case.xv, case.phi.imag, shading='auto')
-			plt.colorbar()
-			plt.figure(figsize=(8, 8))
-			plt.pcolor(case.xv, case.xv, case.phi_.imag, shading='auto')
-			plt.colorbar()
-			plt.figure(figsize=(8, 8))
-			plt.pcolor(case.xv, case.xv, case.phi_gc2_0 - case.phi_gc2_2.real, shading='auto')
-			plt.colorbar()
-			plt.show()
-	elif case.method == 'poincare':
-		y0 = 2.0 * xp.pi * xp.random.rand(2 * case.Ntraj)
-		t_eval = 2.0 * xp.pi * xp.arange(0, case.Tf)
-		start = time.time()
-		sol = solve_ivp(case.eqn_phi, (0, t_eval.max()), y0, t_eval=t_eval, max_step=case.timestep, atol=1, rtol=1)
-		print('\033[92m    Computation finished in {} seconds \033[00m'.format(int(time.time() - start)))
-		if case.modulo:
-			sol.y = sol.y % (2.0 * xp.pi)
-		save_data(case, 'poincare', xp.array([sol.y[:case.Ntraj, :], sol.y[case.Ntraj:, :]]).transpose(), filestr)
-		if case.plot_results:
-			plt.figure(figsize=(8, 8))
-			plt.plot(sol.y[:case.Ntraj, :], sol.y[case.Ntraj:, :], 'b.', markersize=2)
-			plt.show()
-	elif case.method == 'diffusion':
-		y0 = 2.0 * xp.pi * xp.random.rand(2 * case.Ntraj)
-		t_eval = 2.0 * xp.pi * xp.arange(0, case.Tf)
-		start = time.time()
-		sol = solve_ivp(case.eqn_phi, (0, t_eval.max()), y0, t_eval=t_eval, max_step=case.timestep, atol=1, rtol=1)
-		print('\033[92m    Computation finished in {} seconds \033[00m'.format(int(time.time() - start)))
-		r2 = xp.zeros(case.Tf)
-		for t in range(case.Tf):
-			r2[t] += (xp.abs(sol.y[:, t:] - sol.y[:, :case.Tf-t]) ** 2).sum() / (case.Ntraj * (case.Tf - t))
-		diff_data = linregress(t_eval[case.Tf//8:7*case.Tf//8], r2[case.Tf//8:7*case.Tf//8])
-		max_y = (xp.abs(sol.y[:case.Ntraj, :] - sol.y[:case.Ntraj, 0].reshape(case.Ntraj,1)) ** 2\
-		+ xp.abs(sol.y[case.Ntraj:, :] - sol.y[case.Ntraj:, 0].reshape(case.Ntraj,1)) ** 2).max(axis=1)
-		trapped = (max_y <= xp.pi).sum()
-		save_data(case, 'diffusion', [trapped, diff_data.slope, diff_data.rvalue**2], filestr, info='trapped particles / diffusion coefficient / R2')
-		print('\033[96m          trapped particles = {} \033[00m'.format(trapped))
-		print('\033[96m          diffusion coefficient = {:.6f} \033[00m'.format(diff_data.slope))
-		print('\033[96m                     with an R2 = {:.6f} \033[00m'.format(diff_data.rvalue**2))
-		if case.plot_results:
-			plt.figure(figsize=(8, 8))
-			plt.plot(t_eval, r2, 'b', linewidth=2)
-			plt.show()
+class GC2Dk:
+	def __repr__(self):
+		return '{self.__class__.__name__}({self.DictParams})'.format(self=self)
 
+	def __str__(self):
+		return '2D Guiding Center ({self.__class__.__name__}) for the Kryukov-Martinell-delCastilloNegrete potential with FLR = {self.flr_order} and GC order = {self.gc_order}'.format(self=self)
 
-def save_data(case, name, data, filestr, info=[]):
-	if case.save_results:
-		mdic = case.DictParams.copy()
-		mdic.update({'data': data, 'info': info})
-		date_today = date.today().strftime(" %B %d, %Y\n")
-		mdic.update({'date': date_today, 'author': 'cristel.chandre@univ-amu.fr'})
-		name_file = type(case).__name__ + '_' + name + '_' + filestr + '.mat'
-		savemat(name_file, mdic)
-		print('\033[92m    Results saved in {} \033[00m'.format(name_file))
+	def __init__(self, dict_params):
+		for key in dict_params:
+			setattr(self, key, dict_params[key])
+		self.DictParams = dict_params
+		if self.flr_order[0] in range(2):
+			flr1_coeff = 1.0
+		elif self.flr_order[0] == 'all':
+			flr1_coeff = jv(0, self.rho * xp.sqrt(2.0))
+		else:
+			x = sp.Symbol('x')
+			flr_exp = sp.besselj(0, x).series(x, 0, self.flr_order[0] + 1).removeO()
+			flr1_coeff = sp.lambdify(x, flr_exp)(self.rho * xp.sqrt(2.0))
+		self.A1 = self.A * flr1_coeff
+		if self.flr_order[1] in range(2):
+			flr2_coeff20 = 0.0
+			flr2_coeff22 = - 0.25
+		elif self.flr_order[1] == 'all':
+			flr2_coeff20 = - 2.0 * (jv(1, self.rho * 2.0) - xp.sqrt(2.0) *  jv(0, self.rho * xp.sqrt(2.0)) * jv(1, self.rho * xp.sqrt(2.0))) / self.rho
+			flr2_coeff22 = - xp.sqrt(2.0) * (jv(1, self.rho * 2.0 * xp.sqrt(2.0)) - jv(0, self.rho * xp.sqrt(2.0)) * jv(1, self.rho * xp.sqrt(2.0))) / self.rho
+		else:
+			x = sp.Symbol('x')
+			flr2_exp20 = - 2 * ((sp.besselj(1, 2 * x) - sp.sqrt(2) * sp.besselj(0, sp.sqrt(2) * x) * sp.besselj(1, sp.sqrt(2) * x)) / x).series(x, 0, self.flr_order[1] + 1).removeO()
+			flr2_exp22 = - sp.sqrt(2) * ((sp.besselj(1, 2 * sp.sqrt(2) * x) - sp.besselj(0, sp.sqrt(2) * x) * sp.besselj(1, sp.sqrt(2) * x)) / x).series(x, 0, self.flr_order[1] + 1).removeO()
+			flr2_coeff20 = sp.lambdify(x, flr2_exp20)(self.rho)
+			flr2_coeff22 = sp.lambdify(x, flr2_exp22)(self.rho)
+		self.A20 = - self.A ** 2 * self.eta * flr2_coeff20
+		self.A22 = - self.A ** 2 * self.eta * flr2_coeff22
+
+	def eqn_phi(self, t, y):
+		cheby_coeff = eval_chebyu(self.M - 1, xp.cos(t))
+		alpha_b = 0.5 + (xp.cos((self.M + 1) * t) + xp.cos(self.M * t)) * cheby_coeff
+		beta_b = 0.5 + (xp.cos((self.M + 1) * t) - xp.cos(self.M * t)) * cheby_coeff
+		smxy = xp.sin(y[:self.Ntraj] - y[self.Ntraj:])
+		spxy = xp.sin(y[:self.Ntraj] + y[self.Ntraj:])
+		dy_gc1 = self.A1 * xp.array([- alpha_b * smxy + beta_b * spxy, - alpha_b * smxy - beta_b * spxy])
+		if self.gc_order == 1:
+			return dy_gc1.reshape(2 * self.Ntraj)
+		elif self.gc_order == 2:
+			v20 = self.A20 * alpha_b * beta_b * xp.sin(2.0 * y)
+			v2m2 = self.A22 * (alpha_b ** 2) * xp.sin(2.0 * (y[:self.Ntraj] - y[self.Ntraj:]))
+			v2p2 = self.A22 * (beta_b ** 2) * xp.sin(2.0 * (y[:self.Ntraj] + y[self.Ntraj:]))
+			dy_gc2 = 2.0 * xp.array([v20[self.Ntraj:] + v2p2 - v2m2, - v20[:self.Ntraj] - v2p2 - v2m2])
+			return (dy_gc1 + dy_gc2).reshape(2 * self.Ntraj)
 
 
 if __name__ == "__main__":
