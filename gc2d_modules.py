4adbd79793ed810919a9008630d2c5ac16ae679b
Cristel Chandre <cchandre@gmail.com>
2021-10-25 20:18:00 +0200

    Update gc2d_modules.py

--- gc2d_modules.py
+++ gc2d_modules.py
@@ -94,7 +94,7 @@ def run_method(case):
 			y_vec = xp.linspace(0.0, 2.0 * xp.pi, int(xp.sqrt(case.Ntraj)), endpoint=False)
 			y_mat = xp.meshgrid(y_vec, y_vec)
 			y0 = xp.concatenate((y_mat[0].flatten(), y_mat[1].flatten()))
-			case.Ntraj = int(xp.sqrt(case.Ntraj)) ** 2
+			case.Ntraj = int(xp.sqrt(case.Ntraj))**2
 		t_eval = 2.0 * xp.pi * xp.arange(0, case.Tf)
 		start = time.time()
 		sol = solve_ivp(case.eqn_phi, (0, t_eval.max()), y0, t_eval=t_eval, max_step=case.TimeStep, atol=1, rtol=1)
@@ -126,10 +126,10 @@ def run_method(case):
 		if case.Method == 'diffusion':
 			r2 = xp.zeros(case.Tf)
 			for t in range(case.Tf):
-				r2[t] += (xp.abs(sol.y[:, t:] - sol.y[:, :case.Tf-t]) ** 2).sum() / (case.Ntraj * (case.Tf - t))
+				r2[t] += (xp.abs(sol.y[:, t:] - sol.y[:, :case.Tf-t])**2).sum() / (case.Ntraj * (case.Tf - t))
 			diff_data = linregress(t_eval[case.Tf//8:7*case.Tf//8], r2[case.Tf//8:7*case.Tf//8])
-			max_y = (xp.abs(sol.y[:case.Ntraj, :] - sol.y[:case.Ntraj, 0].reshape(case.Ntraj,1)) ** 2\
-				+ xp.abs(sol.y[case.Ntraj:, :] - sol.y[case.Ntraj:, 0].reshape(case.Ntraj,1)) ** 2).max(axis=1)
+			max_y = (xp.abs(sol.y[:case.Ntraj, :] - sol.y[:case.Ntraj, 0].reshape(case.Ntraj,1))**2\
+				+ xp.abs(sol.y[case.Ntraj:, :] - sol.y[case.Ntraj:, 0].reshape(case.Ntraj,1))**2).max(axis=1)
 			trapped = (max_y <= 3.0 * xp.pi).sum()
 			save_data(case, 'diffusion', [trapped, diff_data.slope, diff_data.rvalue**2], filestr, info='trapped particles / diffusion coefficient / R2')
 			print('\033[96m          trapped particles = {} \033[00m'.format(trapped))
