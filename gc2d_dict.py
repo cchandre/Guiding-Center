615f9ad9b204998022642316ff92f4068ac5549b
Cristel Chandre <cchandre@gmail.com>
2022-02-08 16:10:59 +0100

    Update gc2d_dict.py
    - change the format for iterable parameters; now all parameters A, rho and eta are iterable

--- gc2d_dict.py
+++ gc2d_dict.py
@@ -5,29 +5,27 @@
 import numpy as xp
 
 Potential = 'turbulent'
-Method = 'poincare'
+Method = 'diffusion'
 
 FLR = ('all', 'all')
 GCorder = 1
 
-iterable_name = 'rho'
-iterable_values = xp.linspace(0, 0, 1)
 A = 0.7
-#rho = 0.1
+rho = xp.linspace(0, 0.3, 10)
 eta = 0.0
 
 Ntraj = 100
-Tf = 2000
+Tf = 200
 TwoStepIntegration = True
-Tmid = 1000
+Tmid = 100
 TimeStep = 0.05
 init = 'random'
 modulo = False
 threshold = 4
 grid = False
 
-SaveData = True
-PlotResults = False
+SaveData = False
+PlotResults = True
 Parallelization = (False, 34)
 
 darkmode = True
@@ -35,28 +33,25 @@ darkmode = True
 ###################################################################################################
 ##                             DO NOT EDIT BELOW                                                 ##
 ###################################################################################################
-dict_list = [{'Potential': Potential} for _ in range(len(iterable_values))]
-for it in range(len(iterable_values)):
-	dict_list[it].update({iterable_name: iterable_values[it]})
-
 if FLR[0] == 'none' and FLR[1] == 'none':
 	rho = 0
 if GCorder == 1:
 	eta = 0
-
+val_params = xp.meshgrid(A, rho, eta, indexing='ij')
+num_dict = len(val_params[0].flatten())
+dict_list = [{'Potential': Potential} for _ in range(num_dict)]
+for _ in range(num_dict):
+	dict_list[_].update({
+		'A': val_params[0].flatten()[_],
+		'rho': val_params[1].flatten()[_],
+		'eta': val_params[2].flatten()[_]})
 for dict in dict_list:
 	if Potential == 'KMdCN':
 		dict.update({'M': 5})
 	elif Potential == 'turbulent':
 		dict.update({
 			'M': 25,
-			'N': 2 ** 10})
-	if not 'A' in dict:
-		dict.update({'A': A})
-	if not 'rho' in dict:
-		dict.update({'rho': rho})
-	if not 'eta' in dict:
-		dict.update({'eta': eta})
+			'N': 2**10})
 	dict.update({
 		'FLR': FLR,
 		'GCorder': GCorder,
